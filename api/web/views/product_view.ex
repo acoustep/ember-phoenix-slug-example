ad372e8f6588e104ceb23e083ca3f8af6eb5b672
mitchell stanley <ubuntumitch@gmail.com>
2015-11-06 16:42:06 +0000

    Change data to product in JSON attribute

--- api/web/views/product_view.ex
+++ api/web/views/product_view.ex
@@ -2,11 +2,11 @@ defmodule Api.ProductView do
   use Api.Web, :view
 
   def render("index.json", %{products: products}) do
-    %{data: render_many(products, Api.ProductView, "product.json")}
+    %{products: render_many(products, Api.ProductView, "product.json")}
   end
 
   def render("show.json", %{product: product}) do
-    %{data: render_one(product, Api.ProductView, "product.json")}
+    %{product: render_one(product, Api.ProductView, "product.json")}
   end
 
   def render("product.json", %{product: product}) do
